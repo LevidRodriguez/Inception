a0c9d5b27c2486c576ea610c67b07840e0b046a6
Levid Rodriguez <levid.rodriguez@gmail.com>
2020-01-20 14:06:33 -0600

    upload new images

--- bottleneck_features.py
+++ bottleneck_features.py
@@ -35,10 +35,15 @@ bottleneck, image, resized_input = (
 )
 
 # Cargue las imagenes en memoria
-query_image_path = os.path.join(work_dir, 'cat.1000.jpg')
+# query_image_path = os.path.join(work_dir, 'cat.1000.jpg')
+# My Data set
+query_image_path = os.path.join(work_dir, 'DJI_0394.jpg')
 query_image = gfile.FastGFile(query_image_path, 'rb').read()
-target_image_path = os.path.join(work_dir, 'cat.1001.jpg')
+# target_image_path = os.path.join(work_dir, 'cat.1001.jpg') # 16.99647
 # target_image_path = os.path.join(work_dir, 'cropped_panda.jpg') # 20.412373
+# My Data set
+target_image_path = os.path.join(work_dir, 'DJI_0394-1.jpg')
+
 target_image = gfile.FastGFile(target_image_path, 'rb').read()
 
 # Funcion que extrae las caracteristicas bottleneck de la image_data usando la sesion
